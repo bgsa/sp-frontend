653f9e923bb1563e157dbc865550fc95e6ac7559
BrunoGSA <brunogsa@live.com>
2020-07-08 15:59:17 -0300

    Small improvements

--- src/PropertiesFrame.h
+++ src/PropertiesFrame.h
@@ -15,19 +15,81 @@ namespace NAMESPACE_FRONTEND
 	private:
 		sp_uint _selectedObject;
 
+		inline void renderVec3Property(const sp_char* name,  const Vec3& value)
+		{
+			sp_char* text = ALLOC_ARRAY(sp_char, 20);
+
+			ImGui::Text(name);
+			ImGui::NextColumn();
+			ImGui::NextColumn();
+
+			ImGui::Text("x:");
+			ImGui::NextColumn();
+
+			SpString::convert(value.x, text);
+			ImGui::Text(text);
+			ImGui::NextColumn();
+
+			ImGui::Text("y:");
+			ImGui::NextColumn();
+
+			SpString::convert(value.y, text);
+			ImGui::Text(text);
+			ImGui::NextColumn();
+
+			ImGui::Text("z:");
+			ImGui::NextColumn();
+
+			SpString::convert(value.z, text);
+			ImGui::Text(text);
+			ImGui::NextColumn();
+
+			ALLOC_RELEASE(text);
+		}
+
+		inline void renderProperty(const sp_char* name, const sp_float value)
+		{
+			sp_char* text = ALLOC_ARRAY(sp_char, 20);
+			SpString::convert(value, text);
+
+			ImGui::Text(name);
+			ImGui::NextColumn();
+
+			ImGui::Text(text);
+			ImGui::NextColumn();
+
+			ALLOC_RELEASE(text);
+		}
+
+		inline void renderProperty(const sp_char* name, const sp_bool value)
+		{
+			ImGui::Text(name);
+			ImGui::NextColumn();
+
+			if (value)
+				ImGui::Text("True");
+			else
+				ImGui::Text("False");
+
+			ImGui::NextColumn();
+		}
+
 		inline void renderPhysicProperties()
 		{
 			const SpPhysicProperties* physicProperties = SpPhysicSimulator::instance()->physicProperties(_selectedObject);
 
-			ImGui::Text("Position");
-			ImGui::NextColumn();
+			renderProperty("Static", physicProperties->isStatic());
 
-			sp_char* text = ALLOC_ARRAY(sp_char, 20);
-			SpString::convert(physicProperties->position().y, text);
-			ImGui::Text(text);
-			ALLOC_RELEASE(text);
-			
-			ImGui::NextColumn();
+			renderVec3Property("Posision", physicProperties->position());
+			renderVec3Property("Velocity", physicProperties->velocity());
+			renderVec3Property("Acceleration", physicProperties->acceleration());
+			//renderVec3Property("Orientation", physicProperties->orientation());
+			renderVec3Property("Angular Velocity", physicProperties->angularVelocity());
+
+			renderProperty("Mass", ONE_FLOAT / physicProperties->massInverse());
+			renderProperty("Restitution", physicProperties->coeficientOfRestitution());
+			renderProperty("Damping", physicProperties->damping());
+			renderProperty("Friction", physicProperties->coeficientOfFriction());
 		}
 
 	public:
